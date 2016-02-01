c0ed3c889bed22da186891f9955b860643204706
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-02-01 17:22:35 +0000

    Battle initialized

--- app.rb
+++ app.rb
@@ -1,35 +1,9 @@
-require 'sinatra'
-get '/' do
-  "HELLO ARNOLD"
-end
+require 'sinatra/base'
 
-get '/secret' do
-  "NNOOOOOO"
-end
+class Battle < Sinatra::Base
+  get '/' do
+    'Hello Battle!'
+  end
 
-get '/arnold' do
-  ":)"
-end
-
-get '/yes' do
-  'yes'
-end
-
-get '/dss' do
-  'Z SUCKS'
-end
-
-get '/random-cat' do
-  @name = ['amigo', 'oscar', 'viking'].sample
-  erb(:index)
-end
-
-post '/named-cat' do
-  p params
-  @name = params[:name]
-  erb(:index)
-end
-
-get '/cat-form' do
-  erb(:form)
+  run! if app_file == $0
 end
