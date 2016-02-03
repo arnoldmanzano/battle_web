842d2a5034e01acb44ae98fcc87e98ec6978bc0b
Hannah Smyth-Osbourne <h.smythosbourne@gmail.com>
2016-02-03 17:19:08 +0000

    #Closed 26:put the controller on a diet

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,13 +2,20 @@ require 'game'
 
 describe Game do
 
-  subject(:game) {described_class.new}
-  let(:player) {double(:player, reduce_points: nil)}
+  subject(:game) {described_class.new(:player_1, :player_2)}
+  let(:player_1) {double(:player_1, reduce_points: nil)}
+  let(:player_2) {double(:player_2, reduce_points: nil)}
 
   describe '#attack' do
     it 'causes player passed in to reduce points' do
-      expect(player).to receive(:reduce_points)
-      game.attack(player)
+      expect(player_1).to receive(:reduce_points)
+      game.attack(player_1)
+    end
+  end
+
+  describe '#player_1' do
+    it 'returns player_1' do
+      expect(game.player_1).to eq(:player_1)
     end
   end
 end
