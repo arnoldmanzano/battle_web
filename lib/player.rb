4e9809abb835424c956a17de398fad7bb3ea9b06
arnoldmanzano <arnold.manzano@gmail.com>
2016-02-03 15:38:21 +0000

    fixing git repo

--- lib/player.rb
+++ lib/player.rb
@@ -10,12 +10,9 @@ class Player
   def reduce_points
     @points -= 10
   end
-<<<<<<< HEAD
-=======
 
   def attack(player)
     player.reduce_points
   end
->>>>>>> 89e8a432784f13123a5c5aa2f3e043d952752688
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -16,12 +16,6 @@ describe Player do
     end
   end
 
-<<<<<<< HEAD
-  describe '#reduce_points' do
-    it 'reduces the points by 10' do
-      player.reduce_points
-      expect{ player.reduce_points }.to change{ player.points }.by(-10)
-=======
   describe '#attack' do
     it 'causes attacked player to reduce points' do
       player2 = described_class.new('mittens')
@@ -34,7 +28,6 @@ end
     it 'reduces the points by 10' do
       player.reduce_points
       expect{ player.reduce_points }.to change{player.points}.by(-10)
->>>>>>> 89e8a432784f13123a5c5aa2f3e043d952752688
     end
   end
 end
