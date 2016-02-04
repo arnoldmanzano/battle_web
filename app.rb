580c94891d79e7728fa31b3cf8524dc261827dac
Jonathan Pickard <Jonathandpickard@gmail.com>
2016-02-04 14:29:26 +0000

    completed 26

--- app.rb
+++ app.rb
@@ -14,24 +14,24 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $player_1 = Player.new(params[:player_1_name])
-    $player_2 = Player.new(params[:player_2_name])
-    $game = Game.new
+    player_1 = Player.new(params[:player_1_name])
+    player_2 = Player.new(params[:player_2_name])
+    $game = Game.new(player_1, player_2)
     redirect '/play'
   end
 
   get '/play' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
+    @player_1_name = $game.player_1.name
+    @player_2_name = $game.player_2.name
     erb(:play)
   end
 
   get '/attack' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
-    $game.attack($player_2)
-    @player_1_hitpoints = $player_1.hitpoints
-    @player_2_hitpoints = $player_2.hitpoints
+    @player_1_name = $game.player_1.name
+    @player_2_name = $game.player_2.name
+    $game.attack($game.player_2)
+    @player_1_hitpoints = $game.player_1.hitpoints
+    @player_2_hitpoints = $game.player_2.hitpoints
     erb(:attack)
   end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,8 +2,21 @@ require 'game'
 
 describe Game do
 
-  subject(:game) {described_class.new}
+  subject(:game) {described_class.new(player_1, player_2)}
   let(:player_1) {double(:player_1)}
+  let(:player_2) {double(:player_2)}
+
+  describe '#player_1' do
+    it 'returns player 1 after initialize' do
+      expect(game.player_1).to eq player_1
+    end
+  end
+
+  describe '#player_2' do
+    it 'returns player 2 after initialize' do
+      expect(game.player_2).to eq player_2
+    end
+  end
 
   describe '#attack' do
     it 'calls reduce_hitpoints on player' do
