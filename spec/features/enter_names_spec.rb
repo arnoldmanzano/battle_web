86c1c81da4ba0a2af8b7ccd2ba5745e4a3e742ac
Jonathan Pickard <Jonathandpickard@gmail.com>
2016-02-04 09:04:49 +0000

    added web helpers

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -4,10 +4,7 @@ require 'spec_helper'
 
 feature 'Player adds their name' do
 	scenario 'with valid name' do
-		visit('/')
-		fill_in 'player_1_name', with: 'Ina'
-		fill_in 'player_2_name', with: 'Johnny'
-		click_button 'Submit'
-		expect(page).to have_content('Ina vs. Johnny')
+		sign_in_and_play
+		expect(page).to have_content('Ina vs. Jonny')
 	end
 end
--- spec/hit_points_feature_spec.rb
+++ spec/hit_points_feature_spec.rb
@@ -2,10 +2,7 @@ require 'spec_helper'
 
 feature 'View hit points' do
   scenario 'see player 2 hit points' do
-    visit('/')
-    fill_in 'player_1_name', with: 'Ina'
-    fill_in 'player_2_name', with: 'Jonny'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content('Jonny: 60hp')
   end
 end
